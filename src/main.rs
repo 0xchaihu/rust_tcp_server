b62ca9a2598bf483a7327674ac126e806979e4da
bupleurum <bupleurum@outlook.com>
2022-07-06 01:01:04 +0800

    Fix a disconnection issue and format the code.

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,24 @@ fn main() {
     //Creating a `TcpListener` by [`bind`]ing it to a socket address and a port.
     //This demo use addr 127.0.0.1 and port 941
     let listener = TcpListener::bind("127.0.0.1:1941").unwrap();
-    match listener.accept() {
-        //Print the addr of the client.
-        Ok((_socket, addr)) => println!("new client: {addr:?}"),
-        //Print error info if some errors occurs.
-        Err(e) => println!("couldn't get client: {e:?}"),
-    }
 
-    // loop {
-        //iterator over the connections being received on this listener
-        for stream in listener.incoming() {
-            //Get the stream from the enum.
-            let stream = stream.unwrap();
-            //Pass the stream to sub-routine.
-            handle_connection(stream);
-        }
+    // match listener.accept() {
+    //     //Print the addr of the client.
+    //     Ok((_socket, addr)) => println!("new client: {addr:?}"),
+    //     //Print error info if some errors occurs.
+    //     Err(e) => println!("couldn't get client: {e:?}"),
     // }
+
+    //iterator over the connections being received on this listener
+    for stream in listener.incoming() {
+        //Get the stream from the enum.
+        let stream = stream.unwrap();
+        //Print the addr of the client.
+        let addr = stream.peer_addr().unwrap();
+        println!("new client: {addr:?}");
+        //Pass the stream to sub-routine.
+        handle_connection(stream);
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -37,7 +39,10 @@ fn handle_connection(mut stream: TcpStream) {
                 //Send back to the client
                 stream.write(&buffer[0..rec_size]).unwrap();
                 //Print the data in local terminal for debugging
-                println!("The data from client: {}", String::from_utf8_lossy(&buffer[0..rec_size]));
+                println!(
+                    "The data from client: {}",
+                    String::from_utf8_lossy(&buffer[0..rec_size])
+                );
             }
             //If size = 0, no action.
             Ok(_) => {}
